f60b6e943fb73e0a472e5c46d0a5cddde91eb0dc
Tate Wilson <tatewilson4@gmail.com>
2018-04-03 17:53:00 -0600

    added the correct auth form for the create controllers and login controller

--- controllers/sessions.js
+++ controllers/sessions.js
@@ -3,25 +3,33 @@ const router = express.Router();
 const User = require('../models/users.js');
 const bycrypt = require('bcrypt');
 
-router.get('/new' , (req, res) => {
-    res.render('sessions/new.html')
-})
 
-router.post('/' , (req, res) => {
-    User.findOne({ username: req.body.username}, (err, foundUser) => {
-        if(bcyrpt.compareSync(req.body.password, foundUser.password)) {
-            req.session.currentUser = foundUser;
-            res.redirect('/');
+router.post('/', function(req, res){
+    User.findOne({ username: req.body.username }, function(err, foundUser){
+        if( bcrypt.compareSync(req.body.password, foundUser.password) ){
+            req.session.currentuser = foundUser;
+            res.status(201).json({
+                status:201,
+                message:'session created'
+            });
         } else {
-            res.send('wrong password');
+            res.status(401).json({
+                status:401,
+                message:'login failed'
+            });
         }
-    })
-});
-
-router.delete('/' , (req, res) => {
-    req.session.destroy(() => {
-        res.redirect('/');
     });
 });
+
+
+
+router.delete('/', function(req, res){
+    req.session.destroy(function(){
+        res.status(200).json({
+            status:200,
+            message:'logout complete'
+        });
+    });
+})
 
 module.exports = router;
--- controllers/users.js
+++ controllers/users.js
@@ -7,11 +7,15 @@ router.get('/new' , (req, res) => {
     res.render('users/new.html')
 })
 
-router.post('/' , (req, res) => {
+router.post('/', function(req, res){
     req.body.password = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10));
-    User.create(req.body, (err, createdUser) => {
-        res.redirect('/');
+    User.create(req.body, function(err, createdUser){
+        res.status(201).json({
+            status:201,
+            message: "user created"
+        });
     });
 });
+
 
 module.exports = router;
