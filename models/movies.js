cb713a2dc9330d68b5efe49b9cad8c4bb302356f
victorjaquez <victorajaquez@gmail.com>
2018-04-02 19:07:49 -0700

    configured app.js, index.html

---
+++ models/movies.js
@@ -0,0 +1,13 @@
+// const mongoose = require('mongoose');
+//
+// const movieSchema = new mongoose.Schema({
+//     title: String,
+//     actors: String,
+//     genre: String
+//     imdb: String,
+//
+// });
+//
+// const Movies =  mongoose.model('Movie', movieSchema);
+//
+// module.exports = Movies;
